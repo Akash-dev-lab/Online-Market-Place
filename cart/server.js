5d579eddbde9c4888bdc1f421aed4708937200fc
Akash-dev-lab <111779287+Akash-dev-lab@users.noreply.github.com>
2025-10-04 10:47:40 +0530

    Beginning of cart service

---
+++ cart/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const app = require('./src/app');
+const connectDB = require('./db/db');
+
+connectDB();
+
+app.listen(process.env.MONGO_URI, () => {
+  console.log('Cart service is running on port 3002');
+});
